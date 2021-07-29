e784e43c6c424df00d023f9a5e9b27a7e92719d9
ravindukrs <ravindupf@gmail.com>
2021-07-29 14:34:48 +0530

    Add Performance Test

---
+++ test/performance.js
@@ -0,0 +1,19 @@
+import { getPerformanceMetrics } from '@platform-os/testcafe-helpers';
+
+fixture('Speedtest').page("http://localhost:3000/");
+
+test("ebay speedtest", async t => {
+  const perf = await getPerformanceMetrics({t})
+  const computed = perf.computed
+
+  //Time to First Byte
+  //Processing Time
+  //Dom Ready Time 
+
+  console.log(`TTFB: ${computed.ttfb} ms - Processing Time: ${computed.processing} ms -  DOM Ready Time: ${computed.domReady} ms`)
+
+  await t.expect(computed.ttfb).lt(3000)
+  await t.expect(computed.processing).lt(8000)
+  await t.expect(computed.domReady).lt(8000)
+
+})
